3abc6b1dc35dd1e7e626e8abd044edfaa9145916
jatin1812 <125196982+jatin1812@users.noreply.github.com>
2023-03-31 19:10:48 +0530

    Create product.php
    Initial product page for this project.

---
+++ product.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DigiMart</title>
+	<link rel="icon" type="image/x-icon" href="img/favicon2.svg">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+<header>
+	<img src="img/g23.png" height="40px">
+	<<div class="search-container">
+	<div class="search-bar">
+	<input type="search" class="search" placeholder="Search...">
+	<div class="search-btn"><img src="img/search.svg"></div>
+	</div>
+	<img src="img/defdp.jpg" style="height:40px;border-radius:50%">
+	</div>
+</header>
+<div class="grid-cont">
+	<div class="slide-container">
+		<div class ="slide fade">
+			<img src="img/p1.jpg">
+		</div>
+		<div class ="slide fade">
+			<img src="img/p2.jpg">
+		</div>
+		<div class ="slide fade">
+			<img src="img/p3.jpg">
+		</div>
+		<a class="prev" onclick="plusSlide(-1)">&#10094;</a>
+		<a class="next" onclick="plusSlide(1)">&#10095;</a>
+		<div style="text-align: center;margin: 1% 0">
+		<span class="dot" onclick="currentSlide(1)"></span>
+		<span class="dot" onclick="currentSlide(2)"></span>
+		<span class="dot" onclick="currentSlide(3)"></span>
+		</div>
+	</div>
+
+	<div class="desc">
+	<h5>M.R.P:</h5> <h3>₹ 999</h3>
+	<hr>
+	<div class="shop-container">
+	<h4>Qty.: </h4>
+	<select class="qty">
+		<option value="1">1</option>
+		<option value="2">2</option>
+		<option value="3">3</option>
+		<option value="4">4</option>
+		<option value="5">5</option>
+		<option value="6">6</option>
+		<option value="7">7</option>
+		<option value="8">8</option>
+		<option value="9">9</option>
+		<option value="10">10</option>
+	</select>
+	<button class="buy"><img src="img/shop.svg" width="30px" height="30px"><span>Buy</span></button>
+	<button class="add-cart"><img src="img/cart-plus.svg" width="30px" height="30px"><span>Add To Cart</span></button>
+	</div>
+	<h2>layasa Men's Sneakers Walking Shoe</h2>
+		<div style="padding:1% 2%">
+		<h4>Product Description:</h4><br>
+		Elevate your style with this classy pair of Walking Sneaker Shoes for Men from the house of Layasa Brand These Stylish latest Design Sneaker for Men boasts Synthetic Upper with Rubber SOLE for durability These Walking Sneakers are the perfect choice for all those who do not like to compromise on what they wear These Men's Sneaker can be worn for casual and outdoor Purpose. Also best fit for Casual, Outdoor, Fashion and Evening Outing Purpose Layasa Sneakers are made up of High Quality Premium Synthetic material which stays strong and durable - meaning the Sneakers will last much longer Soft Cushioned Insole and Rubber Sole ensures cushioning to the feet removing heel strain Layasa endorses style that strikes a fine balance between the classic and the modern, the discreet and the bold with good taste being the only criterion for selection.
+		</div>
+	</div>
+</div>
+<hr style="margin:1% 3%">
+<div class="details">
+		<h4>Product Details:</h4>
+		<ul style="margin:1%">
+		<li>Product Dimensions : 30 x 12 x 7 cm; 1.6 Kilograms</li>
+		<li>Date First Available : 4 February 2023</li>
+		<li>Manufacturer : LEVEL INTERNATIONAL</li>
+		<li>ASIN : B0BTV827CN</li>
+		<li>Item model number : LKJ2401</li>
+		<li>Country of Origin : India</li>
+		<li>Department : mens</li>
+		<li>Manufacturer : LEVEL INTERNATIONAL</li>
+		<li>Packer : LEVEL INTERNATIONAL</li>
+		<li>Item Weight : 1 kg 600 g</li>
+		<li>Item Dimensions LxWxH : 30 x 12 x 7 Centimeters</li>
+		<li>Net Quantity : 1.00 count</li>
+		<li>Generic Name : Sneaker</li>
+		</ul>
+</div>
+<hr style="margin:1% 1%">
+<script src="js/script.js">
+</script>
+</body>
+</html>
