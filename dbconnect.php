ac3f51e32d247ab3ff213b86b6c411b420b2c92c
jatin1812 <125196982+jatin1812@users.noreply.github.com>
2023-04-10 21:28:16 +0530

    Added 3 PHP files
    added php script file for connecting to database, including header, and php script to print MRP

---
+++ dbconnect.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$pwd = "";
+$db = "digimart";
+$con = new mysqli($servername,$username,$pwd,$db);
+?>
---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+	<img src="img/g23.png" class="logo">
+	<img src="img/g23mob.png" class="logo-mob">
+	<div class="container">
+		<div class="search-container">
+		<input type="search" placeholder="Search..." class="search">
+		<img src="img/search.svg" class="search-btn">
+		<img src="img/defdp.jpg" class="dp">
+		</div>
+	</div>
+</header>
