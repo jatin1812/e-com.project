dd985e555b61ea5637a0c2a3afe5c10aaf6c363b
Sharmaabbhi <131385235+Sharmaabbhi@users.noreply.github.com>
2023-04-22 13:43:31 +0530

    Add files via upload

---
+++ tnc.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Terms and conditions.</title>
+<meta charset="UTF-8" />
+<meta name="viewport" content="initial-scale=1.0,width=device-width">
+<link rel="shortcut icon" href="img/favicon.svg" type="image/x-icon">
+<link rel="stylesheet" href="styletnc.css" type="text/css">
+<style>
+</style>
+</head>
+
+<body>
+    <?php include 'header.php' ?>
+    <div class="terms-box">
+    <div class="terms-text">
+        <h2>Terms of Services</h2>
+    <p> Last Edit:21/04/2023 </p>
+        <P>Greetings Users,</p>
+     <p>We are Digimart, doing business as  (“Digimart,” “we,” “us,” or “our”), a company registered in the State of Himachal Pradesh(INDIA) . We operate the website https://digimart.io (the “Website”) through which we provide you our services, (collectively, the “Services” which include the provision and use of the Website).</p>
+
+<p>You can contact us by phone at (855) 234-50440, by email at digimmart@gmail.com or by post to Digimart.inc., 512civil lines, Dharamshala,Himachal Pradesh(176215).</p>
+
+<p>These Terms of Use constitute a legally binding agreement made between you, whether personally or on behalf of an entity (“you”) and concerning your access to and use of the Website and the Services. You agree that by accessing the Services, you have read, understood, and agree to be bound by all of these Terms of Use. IF YOU DO NOT AGREE WITH ALL OF THESE TERMS OF USE, THEN YOU ARE EXPRESSLY PROHIBITED FROM USING THE SERVICES AND YOU MUST DISCONTINUE USE IMMEDIATELY.</p>
+
+<p>Supplemental terms and conditions or documents that may be posted on the Website from time to time are hereby expressly incorporated herein by reference. We reserve the right, to make changes or modifications to these Terms of Use from time to time. We will alert you about any changes by updating the “Last updated” date of these Terms of Use, and you waive any right to receive specific notice of each such change. It is your responsibility to periodically review these Terms of Use to stay informed as each time you access the Services, you will be subject to, and will be deemed to have been made aware of and to have accepted, the then applicable Terms of Use.</p>
+
+<p>The Services are intended for business users who are at least 18 years old. Persons under the age of 18 are not permitted to use or register for the Services.</p>
+<p>Thankyou</p>
+<h4>I agree to the <span>Terms Of services</span> And I Read The Privacy Notice.</h4>
+<div class="buttons">
+    <button class="btn red-btn">Accept</button>
+    <button class="btn gray-btn">Decline</button>
+</div>
+    
+</body>
+</html>
